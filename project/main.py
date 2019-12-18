a1e75efe6edcc6ce71d87c4144e77bdd299de234
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-12-18 17:36:02 -0600

    Using functions create/list clients

--- project/main.py
+++ project/main.py
@@ -1,6 +1,27 @@
 
-clients = 'pablo, ricardo, '
+clients = 'pablo,ricardo,'
+
+
+def create_client(client_name):
+    global clients
+
+    clients += client_name
+    _add_coma()
+
+
+def list_clients():
+    global clients
+
+    print(clients)
+
+
+def _add_coma():
+    global clients
+
+    clients += ','
+
 
 if __name__ == '__main__':
-    clients += 'hector'
-    print(clients)
+    list_clients()
+    create_client('hector')
+    list_clients()
