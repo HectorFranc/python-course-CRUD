44e4b39cc91d529d34cf7e0fcd09e79ec751679e
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-12-18 18:36:31 -0600

    Options update/delete client has been created

--- project/main.py
+++ project/main.py
@@ -18,6 +18,24 @@ def list_clients():
     print(clients)
 
 
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_client_name + ',')
+    else:
+        _message_client_not_in_client_list()
+
+
+def delete_client(client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', '')
+    else:
+        _message_client_not_in_client_list()
+
+
 def _add_coma():
     global clients
 
@@ -29,18 +47,34 @@ def _print_welcome():
     print('-' * 50)
     print('What would you like to do today?')
     print('[C]reate client')
+    print('[U]pdate')
     print('[D]elete client')
 
+
+def _get_client_name():
+    return input('What is the client name?')
+
+
+def _message_client_not_in_client_list():
+    print('Error: Client is not in clients list')
 
 if __name__ == '__main__':
     _print_welcome()
     command = input('>> ')
+    command = command.upper()
 
     if command == 'C':
-        client_name = input('What is the client name?')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif command == 'D':
-        pass
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the updated client name?')
+        update_client(client_name, updated_client_name)
+        list_clients()
     else:
         print('Invalid command')
