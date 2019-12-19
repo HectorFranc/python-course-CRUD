ecb0fdf1f22c141fd72c739f82a1a489e1c3befa
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-12-19 17:50:20 -0600

    Create/list clients service implementation (using click)

--- managementClientSystem/clients/commands.py
+++ managementClientSystem/clients/commands.py
@@ -1,5 +1,8 @@
 import click
 
+from clients.services import ClientService
+from clients.models import Client
+
 
 @click.group()
 def clients():
@@ -8,17 +11,35 @@ def clients():
 
 
 @clients.command()
+@click.option('-n', '--name', type=str, prompt=True, help='The client name')
+@click.option('-c', '--company', type=str, prompt=True, help='The client company')
+@click.option('-e', '--email', type=str, prompt=True, help='The client email')
+@click.option('-p', '--position', type=str, prompt=True, help='The client position')
 @click.pass_context
 def create(ctx, name, company, email, position):
     """Creates a new client"""
-    pass
+    client = Client(name, company, email, position)
+    client_service = ClientService(ctx.obj['clients_table'])
+    client_service.create_client(client)
 
 
 @clients.command()
 @click.pass_context
 def list(ctx):
     """List all clients"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+    clients_list = client_service.list_clients()
+
+    click.echo(' uid  |  name  |  company  |  email  |  position ')
+    click.echo('*' * 80)
+    for client in clients_list:
+        click.echo(' {uid}  |  {name}  |  {company}  |  {email}  |  {position} '.format(
+            uid=client['uid'],
+            name=client['name'],
+            company=client['company'],
+            email=client['email'],
+            position=client['position']
+        ))
 
 
 @clients.command()
