9081478d1cdc07d241186fae08e275acb1d8ec09
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-12-19 18:40:41 -0600

    Update option implementation

--- managementClientSystem/clients/services.py
+++ managementClientSystem/clients/services.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 from clients.models import Client
 
@@ -18,3 +19,23 @@ class ClientService:
             reader = csv.DictReader(f, fieldnames=Client.schema())
 
             return list(reader)
+
+    def update_client(self, updated_client):
+        clients_list = self.list_clients()
+        updated_clients_list = []
+
+        for client in clients_list:
+            if updated_client.to_dict()['uid'] == client['uid']:
+                updated_clients_list.append(updated_client.to_dict())
+            else:
+                updated_clients_list.append(client)
+        self._save_to_disk(updated_clients_list)
+
+    def _save_to_disk(self, clients_list):
+        tmp_table_name = '{}.tmp'.format(self.table_name)
+        with open(tmp_table_name, mode='w') as f:
+            writer = csv.DictWriter(f, fieldnames=Client.schema())
+            writer.writerows(clients_list)
+
+        os.remove(self.table_name)
+        os.rename(tmp_table_name, self.table_name)
